f20dd635f9d17cebcd6079496b887b67bb4fcabd
Rob Antonisse <robantonisse@gmail.com>
2021-08-04 21:41:09 +0200

    Decoder begrepen en kaal

--- Protocols/DCC_NMRA/DCC_NMRA.ino
+++ Protocols/DCC_NMRA/DCC_NMRA.ino
@@ -14,13 +14,15 @@
 
 #include <NmraDcc.h>
 //***** begin**declaraties decoder NmraDcc
-NmraDcc  Dcc; 
-//DCC_MSG  Packet;
+NmraDcc  Dcc;
 
-// This function is called whenever a normal DCC Turnout Packet is received and we're in Board Addressing Mode
-void notifyDccAccTurnoutBoard(uint16_t BoardAddr, uint8_t OutputPair, uint8_t Direction, uint8_t OutputPower)
-{
-	Serial.print("notifyDccAccTurnoutBoard: ");
+
+
+//***************Call backs, van NmraDcc, voids worden aangeroepen vanuit de library....
+//in toepassing is er maar eentje nodig, rest kan eruit
+void notifyDccAccTurnoutBoard(uint16_t BoardAddr, uint8_t OutputPair, uint8_t Direction, uint8_t OutputPower){
+	//called als CV29 bit6 = false decoderadres,channel,poort,onoff (zie setup 'init')
+	Serial.print(F("dec adres: "));
 	Serial.print(BoardAddr, DEC);
 	Serial.print(',');
 	Serial.print(OutputPair, DEC);
@@ -31,9 +33,9 @@ void notifyDccAccTurnoutBoard(uint16_t BoardAddr, uint8_t OutputPair, uint8_t Di
 }
 
 // This function is called whenever a normal DCC Turnout Packet is received and we're in Output Addressing Mode
-void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower)
-{
-	Serial.print("notifyDccAccTurnoutOutput: ");
+void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputPower){
+	//Called als CV29 bit6=true dccadres,poort,onoff (zie init in setup)
+	Serial.print(F("DCC adres: "));
 	Serial.print(Addr, DEC);
 	Serial.print(',');
 	Serial.print(Direction, DEC);
@@ -42,44 +44,48 @@ void notifyDccAccTurnoutOutput(uint16_t Addr, uint8_t Direction, uint8_t OutputP
 }
 
 // This function is called whenever a DCC Signal Aspect Packet is received
-void notifyDccSigOutputState(uint16_t Addr, uint8_t State)
-{
-	Serial.print("notifyDccSigOutputState: ");
-	Serial.print(Addr, DEC);
-	Serial.print(',');
-	Serial.println(State, HEX);
-}
+//void notifyDccSigOutputState(uint16_t Addr, uint8_t State)
+//deze wordt gebruikt voor seinen, wie weet in de toekomst...
 
+//{
+//	Serial.print("notifyDccSigOutputState: ");
+//	Serial.print(Addr, DEC);
+//	Serial.print(',');
+//	Serial.println(State, HEX);
+//}
+//*******************Einde call backs
 
-void setup()
-{
+void setup(){
 	Serial.begin(9600);
+
+
 	// Setup which External Interrupt, the Pin it's associated with that we're using and enable the Pull-Up 
 	Dcc.pin(0, 2, 1); //interupt number 0; pin 2; pullup to pin2
 	// Call the main DCC Init function to enable the DCC Receiver. 
+	//init van de decoder, vereist, bepaalt hoe en wat van de decoder
+
+	//Dcc.init(MAN_ID_DIY, 10, CV29_ACCESSORY_DECODER | CV29_OUTPUT_ADDRESS_MODE,0);
 	
-	Dcc.init(MAN_ID_DIY, 10, CV29_ACCESSORY_DECODER | CV29_OUTPUT_ADDRESS_MODE,0);
+//		CV29_LOCO_DIR = 0b00000001,	/** bit 0: Locomotive Direction: "0" = normal, "1" = reversed */
+//		CV29_F0_LOCATION = 0b00000010,	/** bit 1: F0 location: "0" = bit 4 in Speed and Direction instructions, "1" = bit 4 in function group one instruction */
+//		CV29_APS = 0b00000100,	/** bit 2: Alternate Power Source (APS) "0" = NMRA Digital only, "1" = Alternate power source set by CV12 */
+//		CV29_RAILCOM_ENABLE = 0b00001000, 	/** bit 3: BiDi ( RailCom ) is active */
+//		CV29_SPEED_TABLE_ENABLE = 0b00010000, 	/** bit 4: STE, Speed Table Enable, "0" = values in CVs 2, 4 and 6, "1" = Custom table selected by CV 25 */
+//		CV29_EXT_ADDRESSING = 0b00100000,	/** bit 5: "0" = one byte addressing, "1" = two byte addressing */
+//		CV29_OUTPUT_ADDRESS_MODE = 0b01000000,	/** bit 6: "0" = Decoder Address Mode "1" = Output Address Mode */
+//		CV29_ACCESSORY_DECODER = 0b10000000,	/** bit 7: "0" = Multi-Function Decoder Mode "1" = Accessory Decoder Mode */
 
-	// Flag values to be logically ORed together and passed into the init() method
-//#define FLAGS_MY_ADDRESS_ONLY        0x01	// Only process DCC Packets with My Address
-//#define FLAGS_AUTO_FACTORY_DEFAULT   0x02	// Call notifyCVResetFactoryDefault() if CV 7 & 8 == 255
-//#define FLAGS_SETCV_CALLED           0x10   // only used internally !!
-//#define FLAGS_OUTPUT_ADDRESS_MODE    0x40  // CV 29/541 bit 6
-//#define FLAGS_DCC_ACCESSORY_DECODER  0x80  // CV 29/541 bit 7
+	//bit 7 en bit 6 worden bovenstaand dus gezet, met dcc adres
+	Dcc.init(MAN_ID_DIY, 10,0b11000000, 0); //idem maar met decoder adres
 
-
+	//bit7 geeft aan loc decoder of accessory decoder
+	//bit 6 true geeft aan of DCCadres terug komt, adres, poort, on/off 
+	//of false decoderadres,channel, poort, on/off
 
 
 	Serial.println(F("DCC"));
 }
-void loop()
-{
-	// You MUST call the NmraDcc.process() method frequently from the Arduino loop() function for correct library operation
+void loop(){
 	Dcc.process();
-	//
-	//if (FactoryDefaultCVIndex && Dcc.isSetCVReady())
-	//{
-	//	FactoryDefaultCVIndex--; // Decrement first as initially it is the size of the array 
-	//	Dcc.setCV(FactoryDefaultCVs[FactoryDefaultCVIndex].CV, FactoryDefaultCVs[FactoryDefaultCVIndex].Value);
-	//}
+
 }
