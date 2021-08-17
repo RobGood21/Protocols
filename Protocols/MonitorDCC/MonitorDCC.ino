d373628695e538507a023a15844665de87a8ab10
Rob Antonisse <robantonisse@gmail.com>
2021-08-17 10:58:56 +0200

    MonitorDCC GFX

--- Protocols/MonitorDCC/MonitorDCC.ino
+++ Protocols/MonitorDCC/MonitorDCC.ino
@@ -10,8 +10,33 @@
 
 */
 
+//libraries
+
+
+//#include <Adafruit_GFX.h>
+#include <Wire.h>
+#include <Adafruit_SSD1306.h>
+
+//constructers
+Adafruit_SSD1306 dp(128, 32, &Wire); // , -1);
 
 void setup() {
+	Serial.begin(9600);
+	if (dp.begin(SSD1306_SWITCHCAPVCC, 0x3C)) {
+		Serial.println(F("DP ok!"));
+	}
+	delay(1000);
+	//test tekst
+	dp.clearDisplay();
+	dp.setTextSize(1);
+	dp.setTextColor(WHITE);
+	dp.setCursor(0, 0);
+	// Display static text
+	dp.println("Dit is de 1e regel");
+	dp.println("en zowaar de 2e regel");
+	dp.println("zeker de derde");
+	dp.print("enne de vierde ook...");
+	dp.display();
 
 }
 
